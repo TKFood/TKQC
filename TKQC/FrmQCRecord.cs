06c1b9b62dc3aa1fc6197647d72facd9fbcc348c
at3158 <tk290@tkfood.com.tw>
2016-09-05 11:53:26 +0800

    add new QC

--- TKQC/FrmQCRecord.cs
+++ TKQC/FrmQCRecord.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TKQC
 {
-    public partial class Form1 : Form
+    public partial class FrmQCRecord : Form
     {
-        public Form1()
+        public FrmQCRecord()
         {
             InitializeComponent();
         }
