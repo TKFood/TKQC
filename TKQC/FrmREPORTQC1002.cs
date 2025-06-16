9d2a34a555adcbbc0f77d6c944c930537913ad28
at3158 <tk290@tkfood.com.tw>
2025-06-16 14:39:23 +0800

    SETFASTREPORT

--- TKQC/FrmREPORTQC1002.cs
+++ TKQC/FrmREPORTQC1002.cs
@@ -70,13 +70,13 @@ namespace TKQC
 
             StringBuilder SQL1 = new StringBuilder();
 
-            SQL1 = SETSQL(SDATES, EDATES);
+            SQL1 = SETSQL2(SDATES, EDATES);
             Report report1 = new Report();
-            report1.Load(@"REPORT\1002.客訴異常處理單報表.frx");
+            report1.Load(@"REPORT\1002.客訴異常處理單明細-橫式.frx");
 
             //20210902密
             Class1 TKID = new Class1();//用new 建立類別實體
-            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbUOF"].ConnectionString);
 
             //資料庫使用者密碼解密
             sqlsb.Password = TKID.Decryption(sqlsb.Password);
@@ -98,53 +98,7 @@ namespace TKQC
             report1.Show();
         }
 
-        public StringBuilder SETSQL(string SDATES, string EDATES)
-        {
-            StringBuilder SB = new StringBuilder();
-
-            SB.AppendFormat(@"                            
-                            SELECT 
-                             [QCFrm002SN] AS '表單編號'
-                            ,[QCFrm002Date] AS '申請日期'
-                            ,[QCFrm002User] AS '申請者'
-                            ,[QCFrm002Dept] AS '部門'
-                            ,[QCFrm002Rank] AS '職級'
-                            ,[QCFrm002CUST] AS '消費者姓名'
-                            ,[QCFrm002TEL] AS '消費者電話'
-                            ,[QCFrm002Add] AS '消費者地址'
-                            ,[QCFrm002CU] AS '供應商/部門單位'
-                            ,[QCFrm002PNO] AS '批號'
-                            ,[QCFrm002CN] AS '品號'
-                            ,[QCFrm002RDate] AS '受理日期'
-                            ,[QCFrm002PRD] AS '品名'
-                            ,[QCFrm002PKG] AS '包裝形式(單片包/罐裝)及規格'
-                            ,[QCFrm002MD] AS '製造日期'
-                            ,[QCFrm002ED] AS '有效日期'
-                            ,[QCFrm002OD] AS '購買日期'
-                            ,[QCFrm002BP] AS '購買地點'
-                            ,[QCFrm002Prove] AS '購買證明'
-                            ,[QCFrm002Abns] AS '客訴原因說明'
-                            ,[QCFrm002Range] AS '使用範圍'
-                            ,[QCFrm002RP] AS '客訴來源'
-                            ,[QCFrm002RD] AS '產品預計回收日'
-                            ,[QCFrm002Abn] AS '客訴原因詳述'
-                            ,[QCFrm002Process] AS '業務處理方式'
-                            ,[QCFrm002QCR] AS '品保建議回覆內容'
-                            ,[QCFrm002ProcessR] AS '業務對外回覆'
-                            ,[QCFrm002QCC] AS '品保判定'
-                            ,[QCFrm002RCAU] AS '判定人員'
-                            ,[QCFrm002PRRD] AS '實際產品回收日期'
-                            ,[QCFrm002Cmf] AS '品保初判'
-                            ,[QCFrm002False] AS '知會人員'
-                            ,[REPORTS] AS '說明'
-                            FROM [TKQC].[dbo].[TBUOFQC1002] WITH(NOLOCK)
-                            WHERE [QCFrm002Date]>='{0}' AND [QCFrm002Date]<='{1}'
-                            ORDER BY [QCFrm002Date]
-                            ", SDATES, EDATES);
-
-            return SB;
-
-        }
+      
 
         public void SETFASTREPORT2(string SDATES, string EDATES)
         {
